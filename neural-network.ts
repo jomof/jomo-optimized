4c7bc85549c63a347777841d69be4c38d9d46e34
Jomo Fisher <JomoFisher@gmail.com>
2015-06-21 10:35:19 -0700

    .

--- neural-network.ts
+++ neural-network.ts
@@ -1,18 +1,21 @@
-function range(high, fn) : any[] {
-    return Array.apply(0, Array(high)).map((_,i)=>fn(i))
+function range(high, fn) {
+	return Array.apply(0, Array(high)).map((_,i)=>fn(i))
 }
 
-/* matrix(5, 1, Math.random)
-
-  [ [ 0.9687679479829967,
-      0.7161620596889406,
-      0.97352325450629,
-      0.06294197984971106,
-      0.9319041615817696 ] ]
-*/
-function matrix(height, width, fn) : any[][] {
-  return range(width, i=>range(height, j=>fn(i, j)))
+function matrix(height, width, fn) : number[][] {
+  return range(height,
+    i=>range(width,
+      j=> fn(i, j)))
 }
+
+function vector(height, fn) : number[][] {
+    return matrix(height, 1, (i,j) => fn(i))
+}
+
+function vectort(width, fn) : number[][] {
+    return matrix(1, width, (i,j) => fn(j))
+}
+
 
 function sigmoid(z) {
     return 1.0/(1.0+Math.exp(-z))
@@ -21,7 +24,7 @@ function sigmoid(z) {
 class Network {
   n : number;
   s : number[];
-  b : number[][];
+  b : number[][][];
   w : number[][][];  
 }
 
@@ -29,41 +32,61 @@ function network(sizes:number[]) {
   var result = new Network()
   result.s = sizes
   result.n = sizes.length
-  result.b = range(sizes.length - 1, i=>range(sizes[i], Math.random))
-  result.w = range(sizes.length - 1, i=>matrix(sizes[i], sizes[i+1], Math.random))
+  result.b = range(sizes.length - 1, i=>vector(sizes[i], Math.random))
+  result.w = range(sizes.length - 1, i=>matrix(sizes[i + 1], sizes[i], Math.random))
   return result
 }
 
-/*
-  a.length is arbitrary
-  a[n].length == b.length
-*/
-function dot(a:number[][], b:number[]) {
-  var result = range(a.length, _=>0)
-  for(var i=0; i<a.length; ++i) {
-    for(var j=0; j<b.length; ++j) {
-      result[i] += (a[i][j] * b[j])
-    }
-  }
+function simple() {
+  var result = new Network()
+  var sizes = [2, 1]
+  result.s = sizes
+  result.n = sizes.length
+  result.b = range(sizes.length - 1, i=>vector(sizes[i], _=>0.5))
+  result.w = range(sizes.length - 1, i=>matrix(sizes[i + 1], sizes[i], _=>0.1))
   return result
 }
 
-function plus(a:number[], b:number[]) {
-  return a.map((_,i)=>a[i] + b[i])
+function dot(a:number[][], b:number[][]) : number[][] {
+  console.assert(a[0].length == b.length, "%s wrong dimension for %s", a, b)
+  return matrix(a.length, b[0].length, (i, j)=> 
+    range(a[0].length, k => a[i][k] * b[k][j])
+      .reduce((p, c)=> p + c, 0))
 }
 
-function feedforward(net : Network, a: number[]) {
-  range(net.n - 1, i=>
+
+function plus(a:number[][], b:number[][]) {
+  return matrix(a.length, a[0].length,
+   (i,j) => a[i][j] + b[i][j])
+}
+
+function feedforward(net : Network, a: number[][]) {
+  vector(net.n - 1, i=>
     a = plus(dot(net.w[i], a), net.b[i])
-          .map(sigmoid))
+         .map(sub=>sub.map(sigmoid))
+          )
   return a;
 }
+//
 
+//console.log(matrix(4, 3, (i, j) => i))
 var net = network([6, 5, 4, 3, 2, 1])
-//console.log(net.w[0])
+//var net = simple()  
+var a = vector(net.s[0], _=> 1)
+//console.log(plus(dot(net.w[0], a), net.b[0]))
+console.log(feedforward(net, vector(net.s[0], _=> 1)))
+
+//console.log(net.b)
 //console.log(net.b[0])
-var a = range(net.s[0], Math.random)
+//var a = [[1, 2, 3], [4, 5, 6]]
+//var b = [[7, 8], [9, 10], [11, 12]]
+//console.log(plus(dot([a], a), [0.5, 0.5]))
+//console.log(dot(a, b))
+
+
+//console.log(feedforward(net, vector(2, _=>1)))
+//var a = range(net.s[0], Math.random)
 //console.log(a)
 //console.log("-----")
-console.log(feedforward(net, a))
+//console.log(feedforward(net, a))
 //console.log(net.b)
